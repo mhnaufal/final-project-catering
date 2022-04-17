5e078ebc146cf830708577b8f9cbd648f041cb35
mhnaufal <aal.pirates16@gmail.com>
2022-04-18 05:35:22 +0700

    init category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_061237) do
+ActiveRecord::Schema.define(version: 2022_04_17_223126) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.text "description", limit: 150
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
