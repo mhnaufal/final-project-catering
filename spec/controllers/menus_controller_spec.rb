785652eac2c8cefdfbab5e75dfc81057c18d6241
mhnaufal <aal.pirates16@gmail.com>
2022-04-17 21:58:33 +0700

    update menu controller test

--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -2,27 +2,37 @@ require 'rails_helper'
 
 RSpec.describe MenusController do
   describe 'GET /menus' do
-    it '[controller.menu.1] it should return all menus' do
+    it '[controller.menu.1] it should return all menus & 200 HTTP code' do
       menu1 = FactoryBot.create(:menu, name: "Nasi Bakar")
       menu2 = FactoryBot.create(:menu, name: "Nasi Uduk")
       menu3 = FactoryBot.create(:menu, name: "Nasi Kuning")
 
       get :index
-      
+
       parsed_body = JSON.parse(response.body)
+
       expect(response).to be_successful
       expect(parsed_body.size).to eq(3)
     end
   end
 
   describe 'GET /menus/:id' do
-    it '[controller.menu.2] it should return a menu with the id' do
+    it '[controller.menu.2] it should return a menu with the id & 200 HTTP code' do
       menu = FactoryBot.create(:menu)
-      
+
       get :show, params: { id: menu }
-      
+
       expect(assigns(:menu)).to eq menu
       expect(response).to be_successful
+    end
+
+    it '[controller.menu.3] it should return empty & 404 HTTP code because there is no menu with the id exist' do
+      get :show, params: { id: 0 }
+
+      parsed_body = JSON.parse(response.body)
+
+      expect(parsed_body).to include({ "message" => "No Menu with that id" })
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
