412502773a8d945b4aba32e31a47e920d9accada
mhnaufal <aal.pirates16@gmail.com>
2022-04-18 05:34:40 +0700

    update menu columns

--- db/migrate/20220417061237_create_menus.rb
+++ db/migrate/20220417061237_create_menus.rb
@@ -1,8 +1,8 @@
 class CreateMenus < ActiveRecord::Migration[7.0]
   def change
     create_table :menus do |t|
-      t.string :name
-      t.text :description
+      t.string :name, unique: true
+      t.text :description, :limit => 150
       t.decimal :price
 
       t.timestamps
