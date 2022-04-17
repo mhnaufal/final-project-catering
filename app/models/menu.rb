82c15eaf7deffcd3e352fd12d160e4912bb0bb17
mhnaufal <aal.pirates16@gmail.com>
2022-04-17 14:24:11 +0700

    menu model

---
+++ app/models/menu.rb
@@ -0,0 +1,5 @@
+class Menu < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true, length: { maximum: 150 }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+end
