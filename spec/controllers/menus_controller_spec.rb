62cc59eb887a5cda05108b46ce33d96b1f82ddab
mhnaufal <aal.pirates16@gmail.com>
2022-04-17 17:19:45 +0700

    menu controller test

---
+++ spec/controllers/menus_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe MenusController do
+  describe 'GET /menus' do
+    it '[controller.menu.1] it should return all menus' do
+      menu1 = FactoryBot.create(:menu, name: "Nasi Bakar")
+      menu2 = FactoryBot.create(:menu, name: "Nasi Uduk")
+      menu3 = FactoryBot.create(:menu, name: "Nasi Kuning")
+
+      get :index
+      
+      parsed_body = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(parsed_body.size).to eq(3)
+    end
+  end
+
+  describe 'GET /menus/:id' do
+    it '[controller.menu.2] it should return menu with the certain id' do
+      menu = FactoryBot.create(:menu)
+
+      get :show, params: { id: menu }
+
+      parsed_body = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(assigns(:menu)).to eq menu
+    end
+  end
+end
