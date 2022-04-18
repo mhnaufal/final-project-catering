ec9e2a91da1a11badef1998638156585ba1e1512
mhnaufal <aal.pirates16@gmail.com>
2022-04-18 07:32:09 +0700

    menu_category model test

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MenuCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'MenuCategory Model' do
+    it '[model.menu_category.0] is valid if factory data valid' do
+      menu_category = FactoryBot.build(:menu_category)
+      expect(menu_category).to be_valid
+    end
+
+    it '[model.menu_category.1] is valid with valid menu_category data' do
+      menu = FactoryBot.build(:menu)
+      category = FactoryBot.build(:category)
+      menu_category = MenuCategory.new(menu: menu, category: category)
+      expect(menu_category).to be_valid
+    end
+  end
 end
