19e32bd0ee09e1d82b1b4a5982ffe1197d1b886e
mhnaufal <aal.pirates16@gmail.com>
2022-04-18 07:34:49 +0700

    add relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,7 @@ class Menu < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true, length: { maximum: 150 }
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+
+    has_many :menu_categories
+    has_many :categories, through: :menu_categories
 end
