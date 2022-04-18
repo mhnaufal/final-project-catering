2124d5c4d0610c4490311ab274312e8151255386
mhnaufal <aal.pirates16@gmail.com>
2022-04-18 07:31:36 +0700

    category model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Category Model' do
+    it '[model.category.0] is valid if factory data valid' do
+      category = FactoryBot.build(:category)
+      expect(category).to be_valid
+    end
+
+    it '[model.category.1] is valid with valid category data' do
+      category = Category.new(name: "Makanan Utama")
+      category.valid?
+      expect(category).to be_valid
+    end
+
+    it '[model.category.2] is not valid without name' do
+      category = FactoryBot.build(:category, name: nil)
+      category.valid?
+      expect(category.errors[:name]).to include("can't be blank")
+      expect(category).to_not be_valid
+    end
+
+    it '[model.category.3] is not valid with duplicated name' do
+      category1 = FactoryBot.create(:category, name: "Makanan Pembuka")
+      category2 = FactoryBot.build(:category, name: "Makanan Pembuka")
+      category2.valid?
+      expect(category2.errors[:name]).to include("has already been taken")
+      expect(category2).to_not be_valid
+    end
+  end
 end
