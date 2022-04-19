14291199ebfd390bf7a2979365016c7a6e04da08
mhnaufal <aal.pirates16@gmail.com>
2022-04-20 06:21:31 +0700

    update menu controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,15 +1,26 @@
 class MenusController < ApplicationController
   def index
     @menus = Menu.all
-    render json: @menus
+    render json: {
+      status: 'SUCCESS',
+      url: request.host || '0.0.0.0',
+      message: 'All available menus',
+      payload: @menus
+    }
   end
 
   def show
     @menu = Menu.find_by_id(params[:id])
+
     if @menu.nil?
       render json: { message: "No Menu with the id = #{params[:id]}" }, status: :not_found
     else
-      render json: @menu
+      render json: {
+        status: 'SUCCESS',
+        url: request.host || '0.0.0.0',
+        message: "Menu with id = #{params[:id]}",
+        payload: @menu
+      }
     end
   end
   
