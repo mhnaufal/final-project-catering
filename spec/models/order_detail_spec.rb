62e4f24d58ed2fd3b2a04fd7c8d53dd6f26b6db8
mhnaufal <aal.pirates16@gmail.com>
2022-04-19 17:20:36 +0700

    add some tests for order detail model

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -36,6 +36,28 @@ RSpec.describe OrderDetail, type: :model do
       order_detail = FactoryBot.build(:order_detail, menu_price: nil)
       order_detail.valid?
       expect(order_detail).to_not be_valid
+      expect(order_detail.errors[:menu_price]).to include("can't be blank")
+    end
+
+    it '[model.order_detail.6] is invalid if menu_price is not in decimal form' do
+      order_detail = FactoryBot.build(:order_detail, menu_price: "$999")
+      order_detail.valid?
+      expect(order_detail).to_not be_valid
+      expect(order_detail.errors[:menu_price]).to include("is not a number")
+    end
+
+    it '[model.order_detail.7] is invalid if quantity is not in integer form' do
+      order_detail = FactoryBot.build(:order_detail, quantity: 9.999)
+      order_detail.valid?
+      expect(order_detail).to_not be_valid
+      expect(order_detail.errors[:quantity]).to include("must be an integer")
+    end
+
+    it '[model.order_detail.8] is invalid if quantity is not greater than or equal to 1' do
+      order_detail = FactoryBot.build(:order_detail, quantity: 0)
+      order_detail.valid?
+      expect(order_detail).to_not be_valid
+      expect(order_detail.errors[:quantity]).to include("must be greater than or equal to 1")
     end
   end
 end
