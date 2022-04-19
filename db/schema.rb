f9af7a244dc16e49679eed82c4d89bec9274d5c5
mhnaufal <aal.pirates16@gmail.com>
2022-04-19 14:34:32 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_101359) do
+ActiveRecord::Schema.define(version: 2022_04_19_073110) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 2022_04_18_101359) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "menu_id", null: false
+    t.integer "quantity"
+    t.decimal "menu_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menu_id"], name: "index_order_details_on_menu_id"
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "email"
     t.decimal "total"
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 2022_04_18_101359) do
 
   add_foreign_key "menu_categories", "categories"
   add_foreign_key "menu_categories", "menus"
+  add_foreign_key "order_details", "menus"
+  add_foreign_key "order_details", "orders"
 end
