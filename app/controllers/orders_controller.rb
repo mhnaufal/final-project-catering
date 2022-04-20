769f4d4c963fff528b6e8d46a66cd03b21124d60
mhnaufal <aal.pirates16@gmail.com>
2022-04-20 12:40:23 +0700

    update order to use json response format

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,21 +6,11 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by_id(params[:id])
-  
+
     if @order.nil?
-      render json: { 
-        status: "NOT_FOUND",
-        url: request.host || '0.0.0.0',
-        message: "No order with the id = #{params[:id]}",
-        payload: @order
-      }, status: :not_found
+      render json: send_failed("No order with the id = #{params[:id]}", nil), status: :not_found
     else
-      render json: { 
-        status: "SUCCESS",
-        url: request.host || '0.0.0.0',
-        message: "Found order with id = #{params[:id]}",
-        payload: @order
-      }
+      render json: send_success("Found order with id = #{params[:id]}", @order)
     end
   end
 end
