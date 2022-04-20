eeae3ef277370a911c6a27ac68ca1afd7f7cc16f
mhnaufal <aal.pirates16@gmail.com>
2022-04-20 12:40:04 +0700

    create json response format helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :send_success
+  helper_method :send_failed
+
+  def send_success(message, payload)
+    {
+      status: "SUCCESS",
+      url: request.host,
+      message: message,
+      payload: payload
+    }
+  end
+
+  def send_failed(message, payload)
+    {
+      status: "FAILED",
+      url: request.host,
+      message: message,
+      payload: payload
+    }
+  end
 end
