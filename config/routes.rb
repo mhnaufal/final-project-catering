ffc5950a7ef1441f3fda5f31d04ba6a545bd22c1
mhnaufal <aal.pirates16@gmail.com>
2022-04-21 22:30:23 +0700

    tidying routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  # Home
   root to: 'home#index'
+  
+  # Menu
   resources :menus
+
+  # Order
   resources :orders
   patch '/orders/status/:id', to: 'orders#change_status'
+
+  # Report
   get '/reports/today', to: 'reports#get_todays_report'
   get '/reports/customer/:email', to: 'reports#get_report_by_email'
   get '/reports/price/:total', to: 'reports#get_report_by_price'
+  get 'reports/date/:date', to: 'reports#get_report_by_date'
 end
